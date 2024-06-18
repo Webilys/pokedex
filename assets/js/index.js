712137f18030e3f82655c6c3b16bf8c748a84a17
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-06-18 14:16:56 +0200

    18/06/24 14:16

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,20 @@
 // affiche la section Bienvenue au chargement de la page
 
-const sectionBienvenue = document.getElementById("Bienvenue");
+const sectionBienvenue = document.getElementById("bienvenue");
 const fichePokemon = document.querySelector(".fichePokemon");
 
 function windowLoad() {
-  sectionBienvenue.style.display = "block";
+  sectionBienvenue.style.display = "flex";
   fichePokemon.style.display = "none";
+}
+
+//Rechercher un pokemon
+const searchPokemon = document.getElementById("searchPokemon");
+const searchPokemonUnit = searchPokemon.value;
+const pokemon = searchPokemonUnit;
+
+//----------- A CORRIGER -------------------
+if (searchPokemonUnit === "pikachu") {
+  sectionBienvenue.style.display = "none";
+  searchPokemonUnit.style.display = "grid";
 }
